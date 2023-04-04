093b816ab98ef9fd46cb98a37a80185b768cad85
asher8k <akassinove1@babson.edu>
2023-04-04 10:28:31 -0400

    Added Documentation

--- Group_homework_4/Group_homework_4.py
+++ Group_homework_4/Group_homework_4.py
@@ -25,9 +25,11 @@ StockTracker Menu:
 def read_directory():
     """
     Read Watchlist Directory
+
     If no directory exists create directory 
     Else return files in directory if anys
     """
+
     if not os.path.exists("watchlists"):
         print("\nNo saved watchlists")
         os.mkdir("watchlists")
@@ -47,10 +49,12 @@ def read_directory():
 def read_list(prompt, option = "open"):
     """
     Read Watchlist File
+
     Parameters: 
         prompt - str: Pass in different prompts for different scenarios
         option - str: Keyword to control for return based on what is needed 
     """
+
     watchlists = read_directory()
     if watchlists:
         choice = int(input(f"{prompt}"))
@@ -65,7 +69,14 @@ def read_list(prompt, option = "open"):
         return print("Select add a list from the menu...")
 
 def track(watchlist):
-    """Track Watchlist"""
+    """
+    Track Watchlist by calling stock API 
+
+    Showing users prices of stocks in their chosen watchlist 
+    Parameter: 
+        watchlist - list: pass read_list() function to get list of stocks in chosen watchlist
+    """
+
     start_time = time()
     prompt = ''
     while True:
@@ -82,6 +93,16 @@ def track(watchlist):
                 break
 
 def add_list():
+    """
+    Allow user to add watchlist 
+
+    Prompt user to enter stock symbols and 
+    append values to symbols list until enter is hit 
+    When enter is hit prompt user to put title of watchlist
+    Control for if title matches existing watchlist title
+    Write symbols out to a new .watchlist file with chosen title
+    """
+
     symbols = []
     print("\nEnter stock symbols then press enter to continue: ")
     
@@ -107,6 +128,16 @@ def add_list():
         file.write(",".join(symbols))
 
 def edit_list():
+    """
+    Allow user to edit existing watchlist
+
+    Unpack tuple returned from read_list() to get file name and the chosen watchlist
+    Prompt user to add or delete symbols and continue to prompt until user enters anything other than a or d:
+    If user adds symbol append symbol to watchlist then write to existing file
+    Elif user deletes symbol show available symbols and remove choice from watchlist
+    Else return to main menu
+    """
+
     watchlist_name, chosen_watchlist = read_list("\nEnter list to edit: ", "edit")
 
     while True:
@@ -134,6 +165,16 @@ def edit_list():
             break
 
 def delete_list():
+    """
+    Allow user to delete existing watchlist 
+
+    Show user available watchlists and prompt them to choose a list to delete
+    Double check that the user want to delete that list: (y/n/other)
+    If yes remove file from watchlist directory
+    Elif show user that watchlist was not deleted and prompt them again
+    Else show user that wathclist was not deleted and return to main menu
+    """
+
     while True: 
         chosen_file = read_list("Enter list to delete: ", "name")
         watchlist_name = chosen_file.replace(".watchlist", "")
@@ -143,9 +184,12 @@ def delete_list():
             os.remove(f"watchlists/{chosen_file}")
             print(f"\n{watchlist_name} watchlist succesfully deleted")
             break
-        else:
+        elif check_deletion == "n": 
             print("\nWatchlist not deleted.\n")
             continue
+        else:
+            print("\nWatchlist not deleted. Returning to main menu...\n")
+            break
 
 options = {
     "1" : track,
@@ -155,6 +199,14 @@ options = {
 }
 
 def main():
+    """
+    Run main logic of the program
+
+    Display menu to user and allow them to choose to 
+    track, add, edit, delete, a watchlist 
+    or quit application
+    """
+
     while True: 
         display_menu()
         choice = input("Enter your selection: ")
